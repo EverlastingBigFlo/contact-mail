b06e9d4d9cb3062750e12638f32a5a3716e5c109
everlastingbigflo <anjorin199@gmail.com>
2024-02-16 14:35:39 +0100

    set my regex

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -81,18 +81,28 @@
                                 to hear from you
                             </p>
 
-                            <form class="mt-6">
+                            <form action="{{ route('')}}" class="mt-6">
+                                @csrf
                                 <div class="flex-1">
                                     <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200 ">Full Name</label>
-                                    <input type="text" placeholder="Full Name"
+                                    <input type="text" name="name" value="{{old('name')}}" placeholder="Full Name"
                                         class="block w-full px-5 py-3 mt-2 text-gray-700 bg-transparent border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
-                                </div>
+                                        <small style="color: red">
+                                            @error('name')
+                                            {{$message}}
+                                        @enderror</small>
+                                    </div>
 
                                 <div class="flex-1 mt-6">
                                     <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email address</label>
-                                    <input type="email" placeholder="Please fill in your email"
+                                    <input type="email" value="{{old('email')}}" placeholder="Please fill in your email"
                                         class="block w-full px-5 py-3 mt-2 text-gray-700 bg-transparent border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
-                                </div>
+                                    <small style="color: red">
+                                            @error('email')
+                                                {{$message}}
+                                            @enderror
+                                    </small>
+                                    </div>
 
                                 <div class="w-full mt-6">
                                     <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Message</label>
@@ -101,7 +111,7 @@
                                         placeholder="Message"></textarea>
                                 </div>
 
-                                <button
+                                <button type="submit"
                                     class="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-red-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50">
                                     get in touch
                                 </button>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 });
 
 Route::get('/contact', [IndexController::class, 'contact'])->name('contact');
+
+
