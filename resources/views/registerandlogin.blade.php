0e731fdc4a32f9ad386044f14b8a85866478dfb1
everlastingbigflo <anjorin199@gmail.com>
2024-02-21 22:17:14 +0000

    21 Feb 2024, 23:17

--- resources/views/registerandlogin.blade.php
+++ resources/views/registerandlogin.blade.php
@@ -246,7 +246,7 @@
                 </div>
                 <div
                     class="img__btn elative overflow-hidden z-10 flex items-center justify-center w-100 h-36 mx-auto 
-                    bg-transparent text-white uppercase text-lg cursor-pointer after:[]">
+                    bg-transparent text-white uppercase text-lg cursor-pointer after:content-['']">
                     content: '';
                     z-index: 2;
                     position: absolute;
